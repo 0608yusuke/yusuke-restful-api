c3e4deb33062c45757f33457b5993c5edadd8f34
0608yusuke <yuyuyuke@icloud.com>
2023-06-15 19:12:37 +0900

    初期設定作り直し

--- src/main/java/com/study/skillup/restfulapi/controller/Controller.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.study.skillup.restfulapi.controller;
-
-public class Controller {
-}
--- src/main/java/com/study/skillup/restfulapi/service/Service.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.study.skillup.restfulapi.service;
-
-public class Service {
-}
