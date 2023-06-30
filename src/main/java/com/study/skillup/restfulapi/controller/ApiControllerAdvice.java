c0c7ff1cdce88c3f3675740d616c605274cd3a0e
0608yusuke <yuyuyuke@icloud.com>
2023-06-30 09:09:20 +0900

    ハンドラークラスの作成

---
+++ src/main/java/com/study/skillup/restfulapi/controller/ApiControllerAdvice.java
@@ -0,0 +1,7 @@
+package com.study.skillup.restfulapi.controller;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+    
+}
