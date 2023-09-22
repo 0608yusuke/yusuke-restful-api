e4b6a895c0051c269c8498d3a8f66589bff569c0
0608yusuke <yuyuyuke@icloud.com>
2023-09-22 13:06:08 +0900

    提出用

--- src/main/java/com/study/skillup/restfulapi/form/ProductForm.java
+++ src/main/java/com/study/skillup/restfulapi/form/ProductForm.java
@@ -1,23 +1,15 @@
 package com.study.skillup.restfulapi.form;
 
-import java.util.Locale;
 import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 
 @Data
 public class ProductForm {
 
-  @Autowired private MessageSource messageSource;
-  String msg = messageSource.getMessage("hello", null, Locale.JAPANESE);
+  @NotBlank private String title;
 
-  @NotBlank(message = "aaa")
-  private String title;
-
-  @NotBlank(message = "{error.products.required}")
-  private String description;
+  @NotBlank private String description;
 
   @Range(min = 1, max = 1000000)
   private Integer price;
